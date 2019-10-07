2b9fae9f20862f9a43da4144d476145a782a8de9
ooeunz <yuns994@gmail.com>
2019-10-07 12:58:21 +0900

    4949 - run time error

--- 4949.py
+++ 4949.py
@@ -1,35 +1,26 @@
-package = {
+if __name__ == "__main__":
+    package = {
         "(" : ")",
         "{" : "}",
         "[" : "]"
     }
-
-def solution(words):
-    brackets = []
-    stack = []
-    words = input()
-    for word in words:
-        if word in package.keys() or word in package.values():
-            brackets.append(word)
-    for bracket in brackets:
-        if not stack:
-            stack.append(bracket)
-        elif bracket in package.keys():
-            stack.append(bracket)
-        elif bracket == package[stack[-1]]:
-            stack.pop()
-
-        if stack:
-            return "no"
-        else:
-            return "yes"
-
-
-
-if __name__ == "__main__":
     while True:
+        brackets = []
+        stack = []
         words = input()
         if words == ".":
             break
-        print(solution(words))
-                
+        for word in words:
+            if word in package.keys() or word in package.values():
+                brackets.append(word)
+        for bracket in brackets:
+            if not stack:
+                stack.append(bracket)
+            elif bracket in package.keys():
+                stack.append(bracket)
+            elif bracket == package[stack[-1]]:
+                stack.pop()
+        print("no" if stack else "yes")
+
+
+            
