abb4c9f1e4c52ca840166d59c95daac157c02d3c
ooeunz <yuns994@gmail.com>
2019-11-02 12:56:04 +0900

    2261 error

--- 2261.py
+++ 2261.py
@@ -1,28 +1,40 @@
 import sys
+import bisect
 from itertools import combinations
 
 
 def distance(pmt):
     return ((pmt[0][0] - pmt[1][0]) ** 2) + ((pmt[0][1] - pmt[1][1]) ** 2)
+
+def compare_x(pmt):
+    return (pmt[0][0] - pmt[1][0]) ** 2
+
+# def compare_y(pmt):
+
+def filter_x(number_of_cases, d):
     
+    x = []
+    for case in number_of_cases:
+        if d > compare_x(case):
+            x.append(case)
+    return x
+    
+def filter_y(number_of_cases, d):
+    number_of_cases.sort(key = lambda x:x[1]) # y 기준으로 정렬
+    start = bisect.bisect_left(number_of_cases, lambda y: y[0][1]-b or y[1][]-b)
+    end = bisect.bisect_left(number_of_cases, y+d)
+
 def solution(points):
-    if len(points) > 5:
-        points.sort(key = lambda x:x[0])
-        mid = len(points) // 2
-        delta = (points[mid][0] + points[mid][0]) / 2
-        
-        if mid < delta:
-            mid = int(delta)
-        
-        left_min = min(map(distance, combinations(points[:mid], 2)))
-        right_min = min(map(distance, combinations(points[mid:], 2)))
+    number_of_cases = list(combinations(points, 2))
 
-        single = min(left_min, right_min)
+    number_of_cases.sort(key = lambda x:x[0])   # x 기준으로 정렬
+    d = distance(number_of_cases[0])
 
-        return min(left_min, right_min, single)
+    x = filter_x(number_of_cases, d)
+    
 
-    else:
-        return min(map(distance, combinations(points, 2)))
+
+
 
 if __name__ == "__main__":
     T = int(sys.stdin.readline())
--- test.py
+++ test.py
@@ -2,20 +2,18 @@ import sys
 from itertools import combinations
 
 
-def solution(points):
-    permutation = list(combinations(points, 2))
+points = [[1, 0], [2, -10], [3, 10], [4, 0], [-2, 3], [4, 7], [2, 7], [5, 6]]
+mid = len(points) // 2
 
-    minimum = float('inf')
-    for pmt in permutation:
-        ab = ((pmt[0][0] - pmt[1][0]) ** 2) + ((pmt[0][1] - pmt[1][1]) ** 2)
-        if ab < minimum:
-            minimum = ab
-    
-    return minimum
 
-if __name__ == "__main__":
-    T = int(sys.stdin.readline())
-    points = [list(map(int, sys.stdin.readline().split())) for _ in range(T)]
+def distance(pmt):
+    return ((pmt[0][0] - pmt[1][0]) ** 2) + ((pmt[0][1] - pmt[1][1]) ** 2)
 
-    # points = [[1, 0], [2, -10], [3, 10], [4, 0]]
-    print(solution(points))
+def each_side(left, right):
+    pair = []
+    for l in left:
+        for r in right:
+            pair.append([l, r])
+    return pair
+
+print(min(map(distance, each_side(points[:mid], points[mid:]))))
