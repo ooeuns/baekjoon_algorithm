3899516bd9708b22bf46a63e7575988c150d81e9
ooeunz <yuns994@gmail.com>
2019-11-08 00:39:25 +0900

    2194 푸는 중

--- 2194.py
+++ 2194.py
@@ -1,32 +1,87 @@
+from collections import deque
+
+
 AROUND = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
 # create map
-row, col = map(int, input().split())
+row, col, A, B, K = map(int, input().split())
 maps = [[1] * col for _ in range(row)]
+dist = [[0] * col for _ in range(row)]
 
 # create unit
-unit = tuple(map(int, input().split()))
+size = (A, B)
 
 # create obstacle
-K = int(input())
 for _ in range(K):
     y, x = map(int, input().split())
     maps[y][x] = 0
 
 # start, end points
-start = tuple(map(int, input().split()))
-end = tuple(map(int, input().split()))
+start = list(map(int, input().split()))
+end = list(map(int, input().split()))
 
 class Unit:
     def __init__(self, size):
         self.start_y, self.start_x = start
         self.size_y, self.size_x = size
         self.current_location = [[self.start_y, self.start_x]]
-        for y in range(1, self.size_y+1):
-            for x in range(1, self.size_x+1):
-                self.current_location.append([self.start_y + y, self.start_x + x])
+        for y in range(self.size_y):
+            for x in range(self.size_x):
+                self.current_location.append([self.start_y - y, self.start_x - x])
     
     def is_arrive(self):
         for location in self.current_location:
             if end in location:
-                return True
+                return dist[self.next_y][self.next_y]
+
+    def move(self, dy, dx):
+        for location in self.current_location:
+            self.y, self.x = location
+            self.next_y = self.y + dy
+            self.next_x = self.x + dx
+
+            # check bounds
+            if not (0 <= self.next_y < row and 0 <= self.next_x < col):
+                return False
+            
+            # check obstacle
+            if not maps[self.next_y][self.next_x]:
+                return False
+        
+        # moving
+        for idx, location in enumerate(self.current_location):
+            self.y, self.x = location
+            self.next_y = self.y + dy
+            self.next_x = self.x + dx
+
+            # record distance
+            dist[self.next_y][self.next_x] = dist[self.y][self.x] + 1
+
+            # move unit
+            self.current_location[idx][0] = self.next_y
+            self.current_location[idx][1] = self.next_x
+        return True
+
+def game():
+    unit = Unit(size)
+    queue = deque([start])
+
+    while queue:
+        x, y = queue.popleft()
+        for dy, dx in AROUND:
+            if unit.move(dy, dx):
+                queue.append((y+dy, x+dx))
+
+            if unit.is_arrive():
+                return unit.is_arrive()
+
+if __name__ == "__main__":
+    print(game())
+    print(dist) # debug
+
+# 5 5 2 2 3
+# 2 2
+# 3 2
+# 3 3
+# 4 1
+# 1 4
--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("helloworld!")
+from collections import Counter
+
+
+counter=[[int(x) for x in input().split()] for y in range(3)]
+
+dic = {1:'A', 2:'B', 3:'C', 4:'D', 0:'E'}
+
+for i in range(3):
+    counter[i] = Counter(counter[i])
+    b = counter[i].get(0)
+    print(dic[b])
+
+# 0 1 0 1
+# 1 1 1 0
+# 0 0 1 1
