adee9a2aec998f566247ac9bc43c172c4af4d779
ooeunz <yuns994@gmail.com>
2019-11-12 01:15:52 +0900

    2194

--- 2194.py
+++ 2194.py
@@ -1,110 +1,55 @@
 from collections import deque
+import sys
 
 
 # initialization
 AROUND = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
 # create map
-row, col, A, B, K = map(int, input().split())
-maps = [[1] * col for _ in range(row)]
-dist = [[0] * col for _ in range(row)]
-visited = [[0] * col for _ in range(row)]
+row, col, size_y, size_x, K = map(int, sys.stdin.readline().split())
 
-# create unit
-size = (A, B)
+maps = [[1] * col for _ in range(row)]
+visited = set()
 
 # create obstacle
 for _ in range(K):
-    y, x = map(int, input().split())
-    maps[y-1][x-1] = 0
+    y, x = map(lambda x: int(x)-1, sys.stdin.readline().split())
+    maps[y][x] = 0
 
 # start, end points
-start = list(map(lambda x: int(x)-1, input().split()))
-end = list(map(lambda x: int(x)-1, input().split()))
+start_y, start_x = map(lambda x: int(x)-1, sys.stdin.readline().split())
+end_y, end_x = map(lambda x: int(x)-1, sys.stdin.readline().split())
 
-visited[start[0]][start[1]] = 1
-
-
-class Unit:
-    def __init__(self, size):
-        self.start_y, self.start_x = start
-        self.size_y, self.size_x = size
-        self.current_location = []
-        for y in range(self.size_y):
-            for x in range(self.size_x):
-                self.current_location.append([(self.start_y) + y, (self.start_x) + x])
-
-    def initialization(self):
-        for location in self.current_location:
-            if not maps[location[0]][location[1]]:
-                return False
-
-    def is_arrive(self):
-        location = self.current_location[0]
-        if end == location:
-            self.end_y, self.end_x = end
-            return dist[self.end_y][self.end_x]
-
-    def move(self, dy, dx):
-        # unit size 전체 check
-        for location in self.current_location:
-            self.y, self.x = location
-            self.next_y = self.y + dy
-            self.next_x = self.x + dx
-
-            # check bounds
-            if not (0 <= self.next_y < row and 0 <= self.next_x < col):
-                return False
-            
-            # print("next_y: ", self.next_y, "self_x: ", self.next_x)
-            # print("maps: ", maps[self.next_y][self.next_x])
-
-            # check obstacle
-            if not maps[self.next_y][self.next_x]:
-                return False
-
-        
-        self.y, self.x = self.current_location[0]
-        self.next_y = self.y + dy
-        self.next_x = self.x + dx
-
-        # check visited
-        if visited[self.next_y][self.next_x]:
-            return False
-
-        # record distance
-        dist[self.next_y][self.next_x] = dist[self.y][self.x] + 1
-        visited[self.next_y][self.next_x] = 1        
-        
-        # moving
-        for idx, location in enumerate(self.current_location):
-            self.y, self.x = location
-            self.next_y = self.y + dy
-            self.next_x = self.x + dx
-
-            # print(dist) # debug
-
-            # move unit
-            self.current_location[idx][0] = self.next_y
-            self.current_location[idx][1] = self.next_x
-        # print(self.current_location)    # debug
-        return True
+def collections(next_y, next_x):
+    # check bounds and obstacle
+    for y in range(size_y):
+        for x in range(size_x):
+            unit_y = next_y + y
+            unit_x = next_x + x
+            if not (0 <= unit_y < row and 0 <= unit_x < col) or not maps[unit_y][unit_x]:
+                return True
+    return False
 
 def game():
-    unit = Unit(size)
-    if not unit.initialization:
-        return -1
-    queue = deque([start])
-
+    queue = deque([[start_y, start_x, 0]])
+    visited.add((start_y, start_x))
     while queue:
-        y, x = queue.popleft()
+        y, x, dist = queue.popleft()
+
         for dy, dx in AROUND:
-            if unit.move(dy, dx):
-                queue.append([y+dy, x+dx])
-                if unit.is_arrive():
-                    return unit.is_arrive()
+            next_y = y + dy
+            next_x = x + dx
+
+            if (next_y, next_x) in visited:
+                continue
+
+            if next_y == end_y and next_x == end_x:
+                return dist + 1
+
+            if not collections(next_y, next_x):
+                queue.append([next_y, next_x, dist+1])
+                visited.add((next_y, next_x))
     return -1
 
 if __name__ == "__main__":
     print(game())
-
--- test.py
+++ test.py
@@ -1,15 +1,47 @@
-from collections import Counter
+import sys
+import math
+import collections
 
+n, m, a, b, k = (map(int, sys.stdin.readline().split()))
+d = [(-1,0), (1,0), (0,-1), (0,1)]
+break_points = set()
+visited = []
+cached = {}
 
-counter=[[int(x) for x in input().split()] for y in range(3)]
+def in_board(p):
+    x, y = p
+    return 1 <= x and x <= n - a + 1 and 1 <= y and y <= m - b + 1
 
-dic = {1:'A', 2:'B', 3:'C', 4:'D', 0:'E'}
+# s, e is a tuple
+def find_min_route(s, e, history = [], depth = 0):
+    queue = collections.deque()
+    visit = set()
+    queue.append((s,0))
+    while queue:
+        cur_pos, cur_step = queue.popleft()
+        if cur_pos in visit:
+            continue
+        # print(f'cur : {cur_pos} cur step : {cur_step}')
+        visit.add(cur_pos)       
+        if cur_pos == e:
+            return cur_step
+        nest = [(cur_pos[0] +dx, cur_pos[1] + dy) for dx, dy in d]
+        possible_nest = [(cs, cur_step+1) for cs in nest if cs not in visit and in_board(cs) and cs not in break_points]
+        # print(possible_nest)
+        queue.extend(possible_nest)
+    return -1
 
-for i in range(3):
-    counter[i] = Counter(counter[i])
-    b = counter[i].get(0)
-    print(dic[b])
+for _ in range(k):
+    bx, by = map(int, sys.stdin.readline().split())
+    break_points.update([(bx-dx, by-dy) for dx in range(0, a) for dy in range(0, b)])
 
-# 0 1 0 1
-# 1 1 1 0
-# 0 0 1 1
+# print(break_points)
+
+fs = tuple(map(int, sys.stdin.readline().split()))
+e = tuple(map(int, sys.stdin.readline().split()))
+
+min_route = find_min_route(fs, e)
+if min_route == math.inf:
+    print("-1")
+else:
+    print(str(min_route))
