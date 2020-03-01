81b4797ff2ad880c7200a6cd06d003ad71dbb427
ooeunz <yuns994@gmail.com>
2020-03-01 23:30:17 +0900

    카잉 달력

--- 6064.py
+++ 6064.py
@@ -1,29 +1,16 @@
 import sys
-
-
-def next_year(M: int, N: int, cur_year: tuple):
-    cur_year = tuple(map(lambda x: x+1, cur_year))
-    x, y, cnt = cur_year
-
-    if x > M:
-        x = 1
-    if y > N:
-        y = 1
-
-    return (x, y, cnt)
+from math import gcd
 
 
 def solution(M: int, N: int, x: int, y: int):
-    cur_year = (1, 1, 1)
+    lcm = M * N // gcd(M, N)
+    x -= 1
+    y -= 1
 
-    while True:
-        cur_x, cur_y, cnt = cur_year
-        if cur_x == x and cur_y == y:
-            return cnt
-        elif cur_x == M and cur_y == N:
-            return -1
-
-        cur_year = next_year(M, N, (cur_x, cur_y, cnt))
+    for idx in range(x, lcm, M):
+        if idx % N == y:
+            return idx + 1
+    return -1
 
 
 if __name__ == "__main__":
