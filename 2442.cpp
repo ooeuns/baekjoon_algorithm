9220387aa4065836add56676d0798a9ddc67c97a
ooeunz <yuns994@gmail.com>
2020-03-04 22:37:42 +0900

    2520

--- 2442.cpp
+++ 2442.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
 
-
-void starStamp(int num) {
-    int zero = num-1;
+void starStamp(int num)
+{
+    int zero = num - 1;
     int star = 1;
-    for(int j=1; j<=num; j++) {
-        for(int i=0; i<zero; i++) {
+    for (int j = 1; j <= num; j++)
+    {
+        for (int i = 0; i < zero; i++)
+        {
             printf(" ");
         }
-        for(int i=0; i<star; i++) {
+        for (int i = 0; i < star; i++)
+        {
             printf("*");
         }
         printf("\n");
         zero--;
-        star = 2*(j+1)-1;
+        star = 2 * (j + 1) - 1;
     }
 }
 
-int main(void) {
-int n;
-scanf("%d", &n);
-starStamp(n);
-return 0;
-
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    starStamp(n);
+    return 0;
 }
---
+++ 2520.py
@@ -0,0 +1,15 @@
+import sys
+from itertools import permutations
+NUMBER = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+def min_chk(N: int, signs: list):
+    return min_num
+
+
+if __name__ == "__main__":
+    N = int(sys.stdin.readline())
+    signs = list(sys.stdin.readline().strip().split())
+
+    print(max_chk(N, signs))
+    print(min_chk(N, signs))
