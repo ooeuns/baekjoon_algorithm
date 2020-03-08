790a58ed13c365229ea20f91d9486f928fb73b7d
ooeunz <yuns994@gmail.com>
2020-03-08 22:44:50 +0900

    14888

--- 14888.py
+++ 14888.py
@@ -1,53 +1,30 @@
 import sys
-from itertools import permutations
 
 
-def calculation(i: int, j: int, k: str):
-    if k == '+':
-        return i + j
-    elif k == '-':
-        return i - j
-    elif k == '/':
-        return int(i / j)
-    elif k == '*':
-        return i * j
+def dfs(num: int, idx: int, add: int, sub: int, multi: int, division: int):
+    if idx == N:
+        global maxv, minv
+        maxv = max(maxv, num)
+        minv = min(minv, num)
+        return
 
-
-def dfs(cur_node: int, ans: int):
-    if cur_node == len(operation):
-        global mx
-        global mn
-
-        if ans > mx:
-            mx = ans
-        if ans < mn:
-            mn = ans
-    else:
-        for i in range(len(operation)):
-            if not visited[i]:
-                visited[i] = True
-                dfs(cur_node+1, calculation(ans, numbers[i+1], operation[i]))
-                visited[i] = False
+    if add:
+        dfs(num + numbers[idx], idx+1, add-1, sub, multi, division)
+    if sub:
+        dfs(num - numbers[idx], idx+1, add, sub-1, multi, division)
+    if multi:
+        dfs(num * numbers[idx], idx+1, add, sub, multi-1, division)
+    if division:
+        dfs(int(num / numbers[idx]), idx+1, add, sub, multi, division-1)
 
 
 if __name__ == "__main__":
-    visited = [False] * 12
-    mx, mn = -1000000000, 1000000000
+    maxv, minv = float('-inf'), float('inf')
 
     N = int(sys.stdin.readline())
     numbers = list(map(int, sys.stdin.readline().split()))
-    ex_operation = list(map(int, sys.stdin.readline().split()))
+    add, sub, multi, division = map(int, sys.stdin.readline().split())
 
-    operation = []
-    operation.extend(['+' for _ in range(ex_operation[0])])
-    operation.extend(['-' for _ in range(ex_operation[1])])
-    operation.extend(['*' for _ in range(ex_operation[2])])
-    operation.extend(['/' for _ in range(ex_operation[3])])
-
-    for i in range(len(operation)):
-        visited[i] = True
-        dfs(1, calculation(numbers[0], numbers[1], operation[i]))
-        visited[i] = False
-
-    print(mx)
-    print(mn)
+    dfs(numbers[0], 1, add, sub, multi, division)
+    print(maxv)
+    print(minv)
