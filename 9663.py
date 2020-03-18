00e884bd9abc17504fc343e575bb186870910945
ooeunz <yuns994@gmail.com>
2020-03-18 13:56:59 +0900

    N-queen -9663

--- 9663.py
+++ 9663.py
@@ -1,9 +1,30 @@
 import sys
 
+ans = 0
 
-def solution():
+
+def is_possible(x: int):
+    for i in range(x):
+        if row[i] == row[x] or abs(row[x] - row[i]) == abs(x - i):
+            return False
+    return True
+
+
+def dfs(x: int):
+    global ans
+
+    if x == N:
+        ans += 1
+    else:
+        for i in range(N):
+            row[x] = i
+            if is_possible(x):
+                dfs(x+1)
 
 
 if __name__ == "__main__":
     N = int(sys.stdin.readline())
-    mtrx = [[0] * N for _ in range(N)]
+    row = [0] * N
+
+    dfs(0)
+    print(ans)
